faa3b047b0b1a7f45d11ee9c194fe27638de1a52
Daniel Mancebo <daniel.m.aldea@hotmail.com>
2024-02-11 17:45:28 +0100

    few scripts and config

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+virtualenv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r $IDF_PATH/requirements.txt
+
+cd build/
+ninja menuconfig
---
+++ flash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ESPPORT="/dev/ttyUSB0"
+
+virtualenv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r $IDF_PATH/requirements.txt
+
+cd build/
+ninja flash
