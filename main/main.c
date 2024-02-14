21b3d85a5499943bb7f1aee191a42df284bb1ce7
Daniel Mancebo <daniel.m.aldea@hotmail.com>
2024-02-14 17:19:16 +0100

    workspace setup

--- main/main.c
+++ main/main.c
@@ -4,19 +4,17 @@
  * @brief
  */
 
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 
 #include <stdio.h>
 
-void app_main(void)
-{
-    ESP_LOGI("app_main", "Starting...");
+void app_main(void) {
+  ESP_LOGI("app_main", "Starting...");
 
-    while (1)
-    {
-        ESP_LOGI("app_main", "Starting...");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+  while (1) {
+    ESP_LOGI("app_main", "Starting...");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
