9a1a0afa864ea9a391c7586bbb06ba1107363dfe
Daniel Mancebo <daniel.m.aldea@hotmail.com>
2025-02-08 13:12:54 +0100

    Use UART0 to set up HC IC

--- main/ble/ble.c
+++ main/ble/ble.c
@@ -6,8 +6,12 @@
  */
 
 #include "FreeRTOS.h"
+#include "projdefs.h"
 #include "task.h"
 
+#include "string.h"
+
+#include "driver/uart.h"
 #include "esp_log.h"
 
 #include "ble.h"
@@ -17,6 +21,9 @@
 // * ----------------------------------------------------------------------------------------------
 
 #define LOG_TAG "BLE_TASK"
+
+#define UART_NUM UART_NUM_0
+#define BUF_SIZE 1024
 
 // * ----------------------------------------------------------------------------------------------
 // * Private Functions Prototypes
@@ -34,6 +41,25 @@ static void _init(void);
 
 static void _init(void)
 {
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    };
+    uart_param_config(UART_NUM, &uart_config);
+    uart_driver_install(UART_NUM, BUF_SIZE, 0, 0, NULL, 0);
+}
+
+static void _read_from_ic(void)
+{
+    uint8_t data[BUF_SIZE];
+    int len = uart_read_bytes(UART_NUM, data, BUF_SIZE - 1, 1000 / portTICK_PERIOD_MS);
+    if (len > 0) {
+        data[len] = '\0'; // Null-terminate the response
+        printf("Received: %s\n", data);
+    }
 }
 
 // * ----------------------------------------------------------------------------------------------
@@ -46,7 +72,11 @@ void ble_task(void* parameters)
     _init();
     ESP_LOGI(LOG_TAG, "initializing activity... ok");
 
+    const char* cmd = "AT\r\n";
     for (;;) {
+        uart_write_bytes(UART_NUM, cmd, strlen(cmd));
+        _read_from_ic();
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 
     vTaskDelete(NULL);
